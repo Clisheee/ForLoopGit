3411e6682c957d44b68c0e3f29d1dcc3f0314367
Opilane TTHK <opilane@tthk.ee>
2024-05-03 12:29:11 +0300

    Create namespace Ifnelse.cs

---
+++ IfUndElse/namespace Ifnelse.cs
@@ -0,0 +1,19 @@
+namespace IfUndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int i = 5;
+            if (i == 5)
+            {
+                Console.WriteLine("Number on 5");
+            }
+            else
+            {
+                Console.WriteLine("Number on midagi muud");
+            }
+        }
+    }
+}
