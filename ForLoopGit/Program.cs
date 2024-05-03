daa0c5bce51a9ff103b8f8acfbe56aa1821ae521
opilane <opilane@HP-CZC0359NNB>
2024-05-03 12:05:55 +0300

    Add project files.

---
+++ ForLoopGit/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForLoopGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            for (int j = 0; j < i; j++)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
